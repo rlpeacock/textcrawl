5d951646e7a8c9e7c29381f7d2d654e60835b47c
Rob Peacock <rob@robpeacock.com>
2023-04-28 22:56:01 -0400

    Removed useless zone field

--- schema/zone.sql
+++ schema/zone.sql
@@ -1,12 +1,9 @@
-
-
 CREATE TABLE IF NOT EXISTS object (
 	   id INTEGER PRIMARY KEY ASC,
 	   attributes TEXT NOT NULL,
 	   title TEXT NOT NULL,
 	   description TEXT NOT NULL,
 	   room INTEGER NOT NULL,
-	   zone INTEGER NOT NULL,
 	   flags INTEGER
 );
 
